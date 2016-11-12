712da7b746d4a73c7b00c45c14fff73c1c1ee2f3
sremedios <sam.remedios@gmail.com>
2016-11-12 04:40:05 -0600

    iteration 1 of Kmeans implementing GenCenterInline

--- ai/KmeansMod.py
+++ ai/KmeansMod.py
@@ -7,7 +7,9 @@ import sys
 import datetime
 from os import listdir
 from os.path import isfile, join
+import operator #used for sorting dict parsing
 
+# for training data
 def GenCenter(songDir,dirSep):
     index = 0
     
@@ -39,34 +41,39 @@ def GenCenter(songDir,dirSep):
 
 # Be sure to pass a measure as argument and not the population
 def GenCenterInline(measure):
-    trainCenter = [0.0,0.0,0.0,0.0]
+    testCenter = [0.0,0.0,0.0,0.0]
     count = 0
     for i in measure: #loop through each note per measure
         for j in measure[i]: #loop through each value per note
-            trainCenter[int(j)] += measure[i][j]
+            testCenter[int(j)] += measure[i][j]
         count += 1
 
     for i in range(4):
-        trainCenter[j] /= count
+        testCenter[j] /= count
 
-    return trainCenter
+    return testCenter
 
-
+# compares centers and returns distance between two points
 def CompCenters(cen1,cen2):
     return math.sqrt((cen2[0]-cen1[0])**2+(cen2[1]-cen1[1])**2+(cen2[2]-cen1[2])**2+(cen2[3]-cen1[3])**2)
 
-# pass entire population
-def ReturnBest(population):
-    fit = {}
+# population is test data, trainingData is used to compare center
+def ReturnBest(population, trainingData):
+    fit = {} #dict of all good measures
+    centers = {} #list of all centers of all measures, to be combed through later
+    topPercent = 0
     for i in population:
-        fit[i] = GenCenterInline(population[i])
+        centers[i] = CompCenters(GenCenterInline(population[i]), trainingData) #mapping measure ID to distance from trainingData
+        sortedCenters = sorted(centers.items(), key=operator.itemgetter(1)) #list of tuples sorted by center value 
+        topPercent += 1 #currently holds total number of measures
+    
+    topPercent *= .2 #20% of total
+    topPercent = int(topPercent)
 
-
-
-cen1 = GenCenter('Train','\\')
-cen2 = GenCenter('Test','\\')
-
-print(CompCenters(cen1,cen2))
+    for i in range(topPercent):
+        fit[sortedCenters[2*i]] = population[sortedCenters[2*i]]
+            
+    return fit
 
 
 
