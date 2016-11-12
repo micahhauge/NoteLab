27c38b739bee74d6f6e2571085966c3a464819fe
sremedios <sam.remedios@gmail.com>
2016-11-12 03:34:30 -0600

    Kmeans in progress

--- ai/KmeansMod.py
+++ ai/KmeansMod.py
@@ -8,20 +8,15 @@ import datetime
 from os import listdir
 from os.path import isfile, join
 
-
-
-
 def GenCenter(songDir,dirSep):
-    index = 0;
-
+    index = 0
     
     musicFiles = [f for f in listdir(songDir) if isfile(join(songDir, f))]
     #print(onlyfiles)
     songIndex = 0
     songProp = []
     trainCenter = [0.0,0.0,0.0,0.0]
-    count = 0;
-    
+    count = 0
     
     for mFile in musicFiles:
         with open(songDir+dirSep+mFile) as f:
@@ -42,9 +37,30 @@ def GenCenter(songDir,dirSep):
 
     return trainCenter
 
+# Be sure to pass a measure as argument and not the population
+def GenCenterInline(measure):
+    trainCenter = [0.0,0.0,0.0,0.0]
+    count = 0
+    for i in measure: #loop through each note per measure
+        for j in measure[i]: #loop through each value per note
+            trainCenter[int(j)] += measure[i][j]
+        count += 1
+
+    for i in range(4):
+        trainCenter[j] /= count
+
+    return trainCenter
+
 
 def CompCenters(cen1,cen2):
     return math.sqrt((cen2[0]-cen1[0])**2+(cen2[1]-cen1[1])**2+(cen2[2]-cen1[2])**2+(cen2[3]-cen1[3])**2)
+
+# pass entire population
+def ReturnBest(population):
+    fit = {}
+    for i in population:
+        fit[i] = GenCenterInline(population[i])
+
 
 
 cen1 = GenCenter('Train','\\')
